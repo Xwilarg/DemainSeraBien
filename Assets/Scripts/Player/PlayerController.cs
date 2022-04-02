3d350db854bea9ec9b1d1839c803ba12b1ac220c
Xwilarg <xwilarg@protonmail.com>
2022-04-02 06:42:11 +0200

    Add Unity pathfinding

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,11 @@
 using LudumDare50.Objective;
 using LudumDare50.SO;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace LudumDare50.Player
 {
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(NavMeshAgent))]
     public class PlayerController : MonoBehaviour
     {
         [SerializeField]
@@ -13,25 +14,28 @@ namespace LudumDare50.Player
         [SerializeField]
         private PlayerInfo _info;
 
-        private Rigidbody _rb;
+        private NavMeshAgent _agent;
 
         private void Start()
         {
-            _rb = GetComponent<Rigidbody>();
+            _agent = GetComponent<NavMeshAgent>();
+            _agent.destination = _destination.transform.position;
         }
 
         private void FixedUpdate()
         {
-            // Keep looking at target
-            transform.LookAt(_destination.transform, Vector3.up);
-
-            // Move toward target
-            _rb.velocity = new Vector3(transform.forward.x * _info.Speed, _rb.velocity.y, transform.forward.z * _info.Speed);
-
             if (Vector3.Distance(transform.position, _destination.transform.position) < _info.MinDistBetweenNode)
             {
+                Debug.Log(_destination.name);
                 _destination = _destination.NextNode;
+                _agent.destination = _destination.transform.position;
             }
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, _destination.transform.position);
         }
     }
 }
