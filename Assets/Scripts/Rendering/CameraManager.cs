cf1c91605f0034e389af08c05ab67315d76d7c58
Xwilarg <xwilarg@protonmail.com>
2022-04-04 22:06:26 +0200

    Add camera zones

---
+++ Assets/Scripts/Rendering/CameraManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace LudumDare50.Rendering
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public static CameraManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        [SerializeField]
+        private Camera _camera;
+
+        [SerializeField]
+        private float _delay;
+
+        private Vector3? _from, _to;
+        private float _timer = 0;
+
+        private void Start()
+        {
+            _from = transform.position;
+            _to = transform.position;
+        }
+
+        public void MoveTo(Vector3 to)
+        {
+            _from = _to;
+            _to = to;
+            _timer = 0;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime * _delay;
+            transform.position = Vector3.Lerp(_from.Value, _to.Value, _timer);
+        }
+    }
+}
---
+++ Assets/Scripts/Rendering/RoomTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LudumDare50.Rendering
+{
+    public class RoomTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _cameraTargetPos;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                CameraManager.Instance.MoveTo(_cameraTargetPos.position);
+            }
+        }
+    }
+}
