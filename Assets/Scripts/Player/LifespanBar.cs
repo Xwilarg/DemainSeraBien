22d8b5e5b847fa4c5227f39b978055e8c6df3b66
Xwilarg <xwilarg@protonmail.com>
2022-04-04 12:05:16 +0200

    Remove useless code

--- Assets/Scripts/Player/LifespanBar.cs
+++ Assets/Scripts/Player/LifespanBar.cs
@@ -6,31 +6,10 @@ namespace LudumDare50.Player
     public class LifespanBar : MonoBehaviour
     {
         public Image fillBar;
-        public Image fillBarSlow;
-
-        private float _value;
-        private float _valueSlow;
-        // private float t = 0;
 
         public void SetValue(float value)
         {
-            // t = 0;
-            _value = value;
-            fillBar.fillAmount = _value;
-            fillBarSlow.fillAmount = _valueSlow;
-        }
-
-
-        void Update()
-        {
-            /*if (_valueSlow != _value)
-            {
-                _valueSlow = Mathf.Lerp(_valueSlow, _value, t);
-                t += 1.0f * Time.deltaTime;
-            }
-
-            fillBar.fillAmount = _value;
-            fillBarSlow.fillAmount = _valueSlow;*/
+            fillBar.fillAmount = value;
         }
     }
 }
