f86e432e4b5c51b4201c96e9f75187c0c507b0e5
Xwilarg <xwilarg@protonmail.com>
2022-04-05 00:10:29 +0200

    Add gameover menu

---
+++ Assets/Scripts/Menu/GameoverMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LudumDare50.Menu
+{
+    public class GameoverMenu : MonoBehaviour
+    {
+        public void GoBackToMainMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
