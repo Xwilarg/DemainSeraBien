4ec90a3adc5de98de826049e20ab57828751513b
Xwilarg <xwilarg@protonmail.com>
2022-04-04 03:01:36 +0200

    Make item script more generic

---
+++ Assets/Scripts/Prop/ItemData.cs
@@ -0,0 +1,12 @@
+﻿using LudumDare50.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Prop
+{
+    public class ItemData : MonoBehaviour
+    {
+        public NeedType TargetNeed;
+
+        public bool IsGood;
+    }
+}
